2646524ba521c450208e503f992d7cec44036b94
Dante-ML <emilianopr.ofi@gmail.com>
2024-07-09 13:27:10 -0600

    .where, validates posterPath

--- lib/infrastrucutre/datasource/tmdb_datasource.dart
+++ lib/infrastrucutre/datasource/tmdb_datasource.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+
 import 'package:movie_viewer/config/constants/environment.dart';
 import 'package:movie_viewer/domain/datasources/movies_datasource.dart';
 import 'package:movie_viewer/domain/entities/movies.dart';
@@ -6,16 +7,10 @@ import 'package:movie_viewer/infrastrucutre/mappers/movie_mapper.dart';
 import 'package:movie_viewer/infrastrucutre/models/moviedb/moviedb_response.dart';
 
 class MoviedbDatasource extends MoviesDatasource {
-
   //Peticiones HTTP para the movie DB
   final dio = Dio(BaseOptions(
-    baseUrl: 'https://api.themoviedb.org/3',
-    queryParameters: {
-      'api_key' : Environment.tmdbKey,
-      'language' : 'es-MX'
-    }
-    )
-  );
+      baseUrl: 'https://api.themoviedb.org/3',
+      queryParameters: {'api_key': Environment.tmdbKey, 'language': 'es-MX'}));
 
   @override
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
@@ -25,12 +20,13 @@ class MoviedbDatasource extends MoviesDatasource {
     final movieDBresponse = MovieDbResponse.fromJson(response.data);
 
     //listado de peliculas
-    final List<Movie> movies = movieDBresponse.results.map(
-      (moviedb) => MovieMapper.movieDBToEntity(moviedb)
-      //Función Lambda que toma todos los objetos de tipo moviedb y los convierte en nuestra entidad movie
-      //utilizando nuestro mapper
-    ).toList();
+    final List<Movie> movies = movieDBresponse.results
+        .where((moviedb) => moviedb.posterPath != 'no-poster')
+        //Función Lambda que toma todos los objetos de tipo moviedb y los convierte en nuestra entidad movie
+        //utilizando nuestro mapper
+        .map((moviedb) => MovieMapper.movieDBToEntity(moviedb))
+        .toList();
 
     return movies;
-  }  
+  }
 }
