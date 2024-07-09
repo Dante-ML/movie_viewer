c1060c41c0fca2b65cf2b34da73b712dc4090afa
Dante-ML <emilianopr.ofi@gmail.com>
2024-07-09 13:20:41 -0600

    Uso del mapper para la respues de TMDB datasource

--- lib/infrastrucutre/datasource/tmdb_datasource.dart
+++ lib/infrastrucutre/datasource/tmdb_datasource.dart
@@ -2,6 +2,8 @@ import 'package:dio/dio.dart';
 import 'package:movie_viewer/config/constants/environment.dart';
 import 'package:movie_viewer/domain/datasources/movies_datasource.dart';
 import 'package:movie_viewer/domain/entities/movies.dart';
+import 'package:movie_viewer/infrastrucutre/mappers/movie_mapper.dart';
+import 'package:movie_viewer/infrastrucutre/models/moviedb/moviedb_response.dart';
 
 class MoviedbDatasource extends MoviesDatasource {
 
@@ -20,8 +22,14 @@ class MoviedbDatasource extends MoviesDatasource {
     //Data directa de TMDB, sin procesar
     final response = await dio.get('/movie/now_playing');
 
+    final movieDBresponse = MovieDbResponse.fromJson(response.data);
+
     //listado de peliculas
-    final List<Movie> movies = [];
+    final List<Movie> movies = movieDBresponse.results.map(
+      (moviedb) => MovieMapper.movieDBToEntity(moviedb)
+      //Función Lambda que toma todos los objetos de tipo moviedb y los convierte en nuestra entidad movie
+      //utilizando nuestro mapper
+    ).toList();
 
     return movies;
   }  
