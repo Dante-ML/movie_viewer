04228f47898d7b896a1d8dbd0744ae93811d6424
Dante-ML <emilianopr.ofi@gmail.com>
2024-07-09 15:11:18 -0600

    movies_providers

---
+++ lib/presentation/providers/movies/movies_providers.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:movie_viewer/domain/entities/movies.dart';
+import 'package:movie_viewer/presentation/providers/movies/movies_repository_provider.dart';
+
+//El notifier es MoviesNotifier y la data que maneja es un listado de Movie
+final nomPlayingMoviesProvider = StateNotifierProvider<MoviesNotifier, List<Movie>>((ref) {
+
+  final fetchMoreMovies = ref.watch(moviesRepositoryProvider).getNowPlaying;
+
+  return MoviesNotifier(fetchMoreMovies: fetchMoreMovies);
+});
+
+typedef MoviesCallBack = Future<List<Movie>> Function({int page});
+
+class MoviesNotifier extends StateNotifier<List<Movie>> {
+  int currentPage = 0;
+  MoviesCallBack fetchMoreMovies;
+
+  MoviesNotifier({
+    required this.fetchMoreMovies,
+  }) : super([]);
+
+  Future<void> loadNextPage() async {
+    currentPage++;
+
+    final List<Movie> movies = await fetchMoreMovies(page: currentPage);
+    state = [...state, ...movies];
+  }
+}
