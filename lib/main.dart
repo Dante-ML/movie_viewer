a7da8caca9eec8b22e1e07ea3d2d8de3e950e8a8
Dante-ML <emilianopr.ofi@gmail.com>
2024-07-09 14:24:44 -0600

    moviesRepositoryProvider implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:movie_viewer/config/router/app_router.dart';
 
 import 'package:movie_viewer/config/theme/app_theme.dart';
 
 Future main() async {
   await dotenv.load(fileName: ".env");
-  runApp(const MainApp());
+  runApp(
+    const ProviderScope(child: MainApp())
+  );
 }
 
 class MainApp extends StatelessWidget {
--- lib/presentation/providers/movies/movies_repository_provider.dart
+++ lib/presentation/providers/movies/movies_repository_provider.dart
@@ -1,2 +1,15 @@
 //Provider que va a crear la instancia del movie Rpository implementation
 
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:movie_viewer/infrastrucutre/datasource/tmdb_datasource_impl.dart';
+import 'package:movie_viewer/infrastrucutre/repositories/movies_repository_impl.dart';
+
+//Provee información a moviesRepository
+final moviesRepositoryProvider = Provider((ref) {
+  //Cambio sencillo entre datasource
+  //return MoviesRepositoryImpl(datasource_generico());
+  //return MoviesRepositoryImpl(IMBDatasource());
+
+  //Repositorio inmutable, proporciona a los demas provider la información para consultar el moviesrepository
+  return MoviesRepositoryImpl(MoviedbDatasource());
+});
